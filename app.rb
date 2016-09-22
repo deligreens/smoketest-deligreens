64f6309ce405b36e5bdf1c3c8dedd56fd654be53
Vincent REHM <vincentrehm@yahoo.fr>
2016-09-22 18:06:49 +0200

    sinatra + stripe

---
+++ app.rb
@@ -0,0 +1,29 @@
+require 'sinatra'
+require 'stripe'
+
+set :publishable_key, ENV['PUBLISHABLE_KEY']
+set :secret_key, ENV['SECRET_KEY']
+
+Stripe.api_key = settings.secret_key
+
+get '/' do
+  erb :index
+end
+
+post '/charge' do
+  @amount = 500
+
+  customer = Stripe::Customer.create(
+    :email => 'customer@example.com',
+    :card  => params[:stripeToken]
+  )
+
+  charge = Stripe::Charge.create(
+    :amount      => @amount,
+    :description => 'Sinatra Charge',
+    :currency    => 'usd',
+    :customer    => customer
+  )
+
+  erb :charge
+end
