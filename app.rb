d82260fffc54d738b059d6e5fb22add6c528c62a
Vincent REHM <vincentrehm@yahoo.fr>
2016-11-26 18:05:53 +0100

    email for 3D cards

--- app.rb
+++ app.rb
@@ -1,6 +1,7 @@
 require 'sinatra'
 require 'stripe'
 require 'dotenv'
+require 'pony'
 
 Dotenv.load
 
@@ -20,6 +21,10 @@ get '/abonnement' do
   erb :abonnement, :layout => false
 end
 
+get '/error' do
+  erb :three_d_secure, :layout => false
+end
+
 post '/bienvenue' do
   @amount = 9000
   @amount_humanize = @amount / 100.0
@@ -29,12 +34,35 @@ post '/bienvenue' do
     :card  => params[:stripeToken]
   )
 
-  charge = Stripe::Charge.create(
-    :amount      => @amount,
-    :description => 'Abonnement Deligreens 6 mois au prix de 3 mois',
-    :currency    => 'eur',
-    :customer    => customer
-  )
+  if customer.cards.first.three_d_secure.supported == "required"
+    email_body = erb :message, :layout => false
+    Pony.options = {
+      :subject => "Erreur dans ton paiement à l'abonnement Deligreens",
+      :body => email_body,
+      :via => :smtp,
+      :via_options => {
+        :address              => 'smtp.gmail.com',
+        :port                 => '587',
+        :enable_starttls_auto => true,
+        :user_name            => 'vincent@deligreens.com',
+        :password             => ENV["SMTP_PASSWORD"],
+        :authentication       => :plain, # :plain, :login, :cram_md5, no auth by default
+        :domain               => "deligreens.com"
+      }
+    }
 
-  erb :charge, :layout => false
+    Pony.mail(:to => params[:stripeEmail])
+
+    erb :three_d_secure, :layout => false
+  else
+    charge = Stripe::Charge.create(
+      :amount      => @amount,
+      :description => 'Abonnement Deligreens 6 mois au prix de 3 mois',
+      :currency    => 'eur',
+      :customer    => customer
+    )
+
+    erb :charge, :layout => false
+  end
+
 end
